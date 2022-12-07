63410ae9adad89e72b209515e08dfeecc939080b
unsivil <unsivilaudio@gmail.com>
2022-12-07 05:50:09 -0700

    changes for netlify prod server

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -1,14 +1,5 @@
-const { PassThrough } = require('stream');
-
-const { Response } = require('@remix-run/node');
-
-const { RemixServer } = require('@remix-run/react');
-
-const isbot = require('isbot');
-
-const { renderToPipeableStream } = require('react-dom/server');
-
-const ABORT_DELAY = 5000;
+import { RemixServer } from '@remix-run/react';
+import { renderToString } from 'react-dom/server';
 
 export default function handleRequest(
     request,
@@ -16,99 +7,14 @@ export default function handleRequest(
     responseHeaders,
     remixContext
 ) {
-    return isbot(request.headers.get('user-agent'))
-        ? handleBotRequest(
-              request,
-              responseStatusCode,
-              responseHeaders,
-              remixContext
-          )
-        : handleBrowserRequest(
-              request,
-              responseStatusCode,
-              responseHeaders,
-              remixContext
-          );
-}
+    const markup = renderToString(
+        <RemixServer context={remixContext} url={request.url} />
+    );
 
-function handleBotRequest(
-    request,
-    responseStatusCode,
-    responseHeaders,
-    remixContext
-) {
-    return new Promise((resolve, reject) => {
-        let didError = false;
+    responseHeaders.set('Content-Type', 'text/html');
 
-        const { pipe, abort } = renderToPipeableStream(
-            <RemixServer context={remixContext} url={request.url} />,
-            {
-                onAllReady() {
-                    const body = new PassThrough();
-
-                    responseHeaders.set('Content-Type', 'text/html');
-
-                    resolve(
-                        new Response(body, {
-                            headers: responseHeaders,
-                            status: didError ? 500 : responseStatusCode,
-                        })
-                    );
-
-                    pipe(body);
-                },
-                onShellError(error) {
-                    reject(error);
-                },
-                onError(error) {
-                    didError = true;
-
-                    console.error(error);
-                },
-            }
-        );
-
-        setTimeout(abort, ABORT_DELAY);
-    });
-}
-
-function handleBrowserRequest(
-    request,
-    responseStatusCode,
-    responseHeaders,
-    remixContext
-) {
-    return new Promise((resolve, reject) => {
-        let didError = false;
-
-        const { pipe, abort } = renderToPipeableStream(
-            <RemixServer context={remixContext} url={request.url} />,
-            {
-                onShellReady() {
-                    const body = new PassThrough();
-
-                    responseHeaders.set('Content-Type', 'text/html');
-
-                    resolve(
-                        new Response(body, {
-                            headers: responseHeaders,
-                            status: didError ? 500 : responseStatusCode,
-                        })
-                    );
-
-                    pipe(body);
-                },
-                onShellError(err) {
-                    reject(err);
-                },
-                onError(error) {
-                    didError = true;
-
-                    console.error(error);
-                },
-            }
-        );
-
-        setTimeout(abort, ABORT_DELAY);
+    return new Response('<!DOCTYPE html>' + markup, {
+        headers: responseHeaders,
+        status: responseStatusCode,
     });
 }
--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,13 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
+    serverBuildTarget: 'netlify',
+    server:
+        process.env.NETLIFY || process.env.NETLIFY_LOCAL
+            ? './server.js'
+            : undefined,
+    ignoredRouteFiles: ['**/.*'],
+    // appDirectory: "app",
+    // assetsBuildDirectory: "public/build",
+    // serverBuildPath: ".netlify/functions-internal/server.js",
+    // publicPath: "/build/",
 };
